b9cb79587f5db36a8ad22baad225964e08bdfc50
Diego Sanabria Dacosta <dsanabria@deltacontadores.com>
2020-12-22 18:25:40 -0600

    Uso de switch case

---
+++ switch_case/bin/switch_case.dart
@@ -0,0 +1,20 @@
+void main(List<String> args) {
+  int opcion;
+
+  opcion = 3;
+
+  switch (opcion) {
+    case 1:
+      print('Estas el la opción 1');
+      break;
+    case 2:
+      print('Estas el la opción 2');
+      break;
+    case 3:
+      print('Estas el la opción 3');
+      break;
+    default:
+      print('No estas en ninguna opción');
+      break;
+  }
+}
